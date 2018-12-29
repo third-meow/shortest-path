a76b571db9dbba955c0e368cfb0e38a0af54f497
The third meow <jsheaandcats@gmail.com>
2018-12-29 22:39:28 +1300

    added test script

---
+++ create_test_case.py
@@ -0,0 +1,20 @@
+
+def simple():
+    node_n = 100000
+    edges = []
+    for i in range(1, node_n):
+        edges.append([i-1, i])
+        edges.append([i, 0])
+        edges.append([i, i-1])
+
+    return edges, 0, node_n, node_n
+
+edges, start, target, node_n = simple()
+
+with open('data.txt', 'w') as f:
+    f.write(f'{node_n}\n')
+    f.write(f'{start} {target}\n')
+    for edge in edges:
+        f.write(f'{edge[0]} {edge[1]}\n')
+
+
