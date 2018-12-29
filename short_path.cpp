8d6e71e4e8fb91d386d5f436a7fc72e752669b32
The third meow <jsheaandcats@gmail.com>
2018-12-29 16:17:29 +1300

    oop style, still works 40%

--- short_path.cpp
+++ short_path.cpp
@@ -1,97 +1,115 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> edges;
-
-template <typename T>
-bool contains(vector<T> vec, T element) {
-	for(auto e : vec) {
-		if (e == element) {
-			//cout << "conts" << endl;
-			return true;
-		}
-	}
-
-	//cout << "doesnt cont" << endl;
-	return false;
-}
-
 struct sort_edges_key{
     inline bool operator() (const vector<int> a, const vector<int> b) {
 			if (a[0] == b[0]) {
-				return (a[1] > b[1]);
+				return (a[1] < b[1]);
 			} else {
-				return (a[0] > b[0]);
+				return (a[0] < b[0]);
 			}
     }
 };
 
-struct sort_options_key {
-    inline bool operator() (const vector<int> a, const vector<int> b) {
-					return (a.size() <  b.size());
-    }
+class Crawler {
+	private:
+		vector<int> _path;
+		int _node;
+	public:
+		bool active;
+
+		Crawler(int start_node) {
+			active = true;
+			_node = start_node;
+			_path = {_node};
+		}
+
+		Crawler(int start_node, vector<int> start_path) {
+			active = true;
+			_node = start_node;
+			_path = start_path;
+			_path.push_back(_node);
+		}
+
+		bool step();
 };
 
-vector<int> best_path(int end, int start, vector<int> used) {
-	cout << "<>"  << endl;
-	if (start == end)
-		return {start};
+vector<Crawler> crawlers;
+vector<vector<int>> edges;
+vector<int> seen;
+int target;
 
-	//search for direct hits
-	for(auto vec : edges) {
-		if (vec[0] == start) {
-			if (vec[1] == end) {
-				//found it
-				return {end, start};
-			}
-		}
-	}
-
-	//send search for recursive hits
-	vector<vector<int>> options;
-	used.push_back(start);
-	for(auto vec : edges) {
-		if (vec[0] == start) {
-			if (!contains<int>(used, vec[1])) {
-				options.push_back(best_path(end, vec[1], used));
-			}
-		}
-	}
-		
-	sort(options.begin(), options.end(), sort_options_key());
-	options[0].push_back(start);
-	return options[0];
+bool contains(int element) {
+	for(auto e : seen)
+		if (e == element)
+			return true;
+	return false;
 }
 
+bool Crawler::step() {
+	vector<int> unseen_neighbours;
+	for(auto e : edges) {
+		if(e[0] == _node) {
+			if(e[1] == target) {
+				for(auto v : _path)
+					cout << v << " ";
+				cout << target;
+				return true;
+			}
+
+			if(!contains(e[1]))
+					unseen_neighbours.push_back(e[1]);
+		}
+	}
+
+	if (unseen_neighbours.size()) {
+		for(int i = 1; i < unseen_neighbours.size(); ++i) {
+			crawlers.push_back(Crawler(unseen_neighbours[i], _path));
+			seen.push_back(unseen_neighbours[i]);
+		}
+		_node = unseen_neighbours[0];
+		_path.push_back(_node);
+	} else {
+		active = false;
+	}
+
+	return false;
+}
+
+
 int main(int argc, char *argv[]) {
-	int node_n, start, end;
+	int node_n, start;
 	cin >> node_n;
-	cin >> start >> end;
+	cin >> start >> target;
 
 	string line;
 	int a,b;
 	getline(cin, line); //TRASH FIRST LINE
 	while(getline(cin, line)) {
 		sscanf(line.data(), "%d %d\n", &a, &b);
-
 		vector<int> edge = {a,b};
 		edges.push_back(edge);
 	}
 
 	sort(edges.begin(), edges.end(), sort_edges_key());
 
-	for(auto e : edges)
-		cout << e[0] << " " << e[1] << endl;
-
-	if (start == end) {
+	if (start == target) {
 		cout << start;
 	} else {
-		vector<int> path = {};
-		path = best_path(end, start, path);
+		crawlers.push_back(Crawler(start));
 
-		cout << path.back();
-		for(int i = (path.size() - 2); i >= 0; --i)
-			cout << " " << path[i];
+		bool found;
+		while (true){
+			for(int i = 0; i < crawlers.size(); ++i){
+				if (crawlers[i].active) {
+					found = crawlers[i].step();
+					if (found) break;
+				}else{
+					crawlers.erase(crawlers.begin() + i);
+				}
+			}
+			if(found) break;
+		}
 	}
 
 	return 0;
