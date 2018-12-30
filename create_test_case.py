a0feea6f29dd8e85d0b276c98d270480fb5c57a5
The third meow <jsheaandcats@gmail.com>
2018-12-30 15:30:16 +1300

    add star graph

--- create_test_case.py
+++ create_test_case.py
@@ -2,14 +2,23 @@
 def simple():
     node_n = 100000
     edges = []
-    for i in range(1, node_n):
+    for i in range(1, node_n + 1):
         edges.append([i-1, i])
         edges.append([i, 0])
         edges.append([i, i-1])
 
     return edges, 0, node_n, node_n
 
-edges, start, target, node_n = simple()
+def star():
+    node_n = 100000
+    edges = []
+    for i in range(1, node_n + 1):
+        edges.append([0, i])
+
+    return edges, 0, node_n, node_n
+
+
+edges, start, target, node_n = star()
 
 with open('data.txt', 'w') as f:
     f.write(f'{node_n}\n')
