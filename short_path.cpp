409ca5809b0ac91ee79dbb6685e27f9907beba74
The third meow <jsheaandcats@gmail.com>
2018-12-31 22:49:48 +1300

    80%, mem now issue

--- short_path.cpp
+++ short_path.cpp
@@ -13,8 +13,7 @@ struct Timer {
 	string name;
 	chrono::time_point<chrono::high_resolution_clock> start;
 	chrono::time_point<chrono::high_resolution_clock> end;
-	chrono::duration<double> duration;
-
+	chrono::duration<double> duration; 
 
 	Timer() {
 		name = "unnamed";
@@ -36,7 +35,7 @@ struct Timer {
 		duration = end - start;
 
 		double ms = duration.count() * 1000.0f;
-		if (name.size()) 
+		if (name.size())
 			cout << "timer: " << name << " took: " << ms << endl;
 		else if (vec != nullptr)
 			vec->push_back(ms);
@@ -44,7 +43,7 @@ struct Timer {
 };
 
 struct sort_edges_key{
-    inline bool operator() (const vector<int> a, const vector<int> b) {
+    inline bool operator() (const vector<int32_t> a, const vector<int> b) {
 			if (a[0] == b[0]) {
 				return (a[1] < b[1]);
 			} else {
@@ -54,77 +53,59 @@ struct sort_edges_key{
 };
 
 struct Node {
-		int id;
-		int dis;
-		vector<int> path;
+		int32_t id;
+		int32_t dis;
+		Node* prev;
 
-		Node(int init_id) {
-			id = init_id;
-			dis = -1;
-			path = {id};
+		Node() {
+			id = dis = -1;
+			prev = nullptr;
 		}
 
-		Node(int init_id, int init_dis) {
+		Node(int32_t init_id, int32_t init_dis) {
 			id = init_id;
 			dis = init_dis;
-			path = {id};
-		}
-
-		Node(int init_id, int init_dis, vector<int> init_path) {
-			id = init_id;
-			dis = init_dis;
-			path = init_path;
-			path.push_back(id);
+			prev = nullptr;
 		}
 };
 
 class Queue {
 	public:
-		vector<Node> data;
+		Node data[200000];
+		int32_t begin, end;
+
+		//constructor
 		Queue(Node init_node);
-		//sort by node dis
-		void sort();
 		//adds element to back
-		void push(Node);
+		void push(Node n);
 		//returns (and removes) element at front
-		Node pop();
+		Node* pop();
 };
 
 Queue::Queue(Node init_node) {
-	Timer t("queue construct");
-	data = {init_node};
-	data[0].path = {data[0].id};
-	data.reserve(1000000);
+	data[0] = init_node;
+	data[0].prev = &data[0];
+	begin = 0;
+	end = 1;
 }
 
-void Queue::sort() {
-	Timer t(&sort_times);
-	struct data_sort_key{
-			inline bool operator() (const Node a, const Node b) {
-					return (a.dis > b.dis);
-			}
-	};
-
-	std::sort(data.begin(), data.end(), data_sort_key());
+void Queue::push(Node n) {
+	//Timer t(&push_times);
+	n.prev = &data[begin-1];
+	data[end] = n;
+	++end;
 }
 
-
-void Queue::push(Node n){
-	Timer t(&push_times);
-	data.push_back(n);
-}
-
-Node Queue::pop() {
-	Timer t(&pop_times);
-	Node n = data.back();
-	data.pop_back();
-	return n;
+Node* Queue::pop() {
+	//Timer t(&pop_times);
+	++begin;
+	return &data[begin-1];
 }
 
 
-vector<vector<int>> edges;
-vector<vector<int>> adj_list;
-int target;
+vector<vector<int32_t>> edges;
+vector<vector<int32_t>> adj_list;
+int32_t target, start;
 
 double avgVec(vector<double>* vec) {
 	double total;
@@ -153,35 +134,47 @@ void timehere() {
 	gettimeofday(&tv,NULL);
 	u64useconds = (1000000*tv.tv_sec) + tv.tv_usec;
 
-	cout << u64useconds << endl;
+	//cout << u64useconds << endl;
 }
 
+void foundOut(Node* n) {
+	int dis = n->dis;
+	int path[dis];
+	for(int i = 0; i < dis; ++i) {
+		path[i] = n->id;
+		n = n->prev;
+	}
+	cout << start << " ";
+	for(int i = (dis-1); i >= 0; --i)
+		cout << path[i] << " ";
+	cout << target;
+}
 
 int main(int argc, char *argv[]) {
 
-	int vertex_n, start;
+	int32_t vertex_n;
 	cin >> vertex_n;
 	bool notseen[vertex_n];
-	for(int i = 0; i < vertex_n; ++i)
+	for(int32_t i = 0; i < vertex_n; ++i)
 		notseen[i] = true;
 
 	cin >> start >> target;
 
 	{
-		Timer t("read-input");
+		//Timer t("read-input");
 		string line;
-		int a,b;
+		int32_t a,b;
 		getline(cin, line); //TRASH FIRST LINE
 		while(getline(cin, line)) {
 			sscanf(line.data(), "%d %d\n", &a, &b);
-			vector<int> edge = {a,b};
+			vector<int32_t> edge = {a,b};
 			edges.push_back(edge);
 		}
 	}
 
 	{
-		Timer t("build adj list");
-		for(int i = 0; i < vertex_n; ++i)
+		//Timer t("build adj list");
+		for(int32_t i = 0; i < vertex_n; ++i)
 			adj_list.push_back({});
 
 		for(auto e : edges)
@@ -191,51 +184,46 @@ int main(int argc, char *argv[]) {
 
 	long loop_count = 0;
 	if (start == target) {
-		//cout << "found";
 		cout << start << endl;
 	} else {
 		{
-			Timer t("shortest path algo");
+			//Timer t("shortest path algo");
 			Queue q(Node(start, 0));
 
 			bool found = false;
 			while (!found) {
-				Timer t(&loop_times);
+				//Timer t(&loop_times);
 
-				Node n = q.pop();
+				Node* n = q.pop();
 
 
-				for(int connected : adj_list[n.id]) {
+				for(int32_t connected : adj_list[n->id]) {
 					if (connected == target) {
-						//for(auto v: n.path) 
-							//cout << v << " ";
-						//cout << target;
-						cout << "found" << endl;
+						foundOut(n);
 						found = true;
 						break;
 					} else if (notseen[connected]){
 						notseen[connected] = false;
-						q.push(Node(connected, n.dis+1, n.path));
+						q.push(Node(connected, n->dis+1));
 					}
 				}
-				q.sort();
 				++loop_count;
 			}
 		}
 
-		cout << endl;
-		cout << "avg loop time: " << avgVec(&loop_times) << endl;
-		cout << "loop count: " << loop_count << "  total: " << totalVec(&loop_times) << endl;
+		//cout << endl;
+		//cout << "avg loop time: " << avgVec(&loop_times) << endl;
+		//cout << "loop count: " << loop_count << "  total: " << totalVec(&loop_times) << endl;
 
-		cout << "avg sort time: " << avgVec(&sort_times) << endl;
-		cout << "sorts: " << sort_times.size() << " total : " << totalVec(&sort_times) << endl;
+		//cout << "avg sort time: " << avgVec(&sort_times) << endl;
+		//cout << "sorts: " << sort_times.size() << " total : " << totalVec(&sort_times) << endl;
 
-		cout << "avg pop time: " << avgVec(&pop_times) << endl;
-		cout << "pops: " << pop_times.size() << " total : " << totalVec(&pop_times)  << endl;
+		//cout << "avg pop time: " << avgVec(&pop_times) << endl;
+		//cout << "pops: " << pop_times.size() << " total : " << totalVec(&pop_times)  << endl;
 
-		cout << "avg push time: " << avgVec(&push_times) << endl;
-		cout << "pushs: " << push_times.size() << " total : " << totalVec(&push_times) << endl;
-
+		//cout << "avg push time: " << avgVec(&push_times) << endl;
+		//cout << "pushs: " << push_times.size() << " total : " << totalVec(&push_times) << endl;
 	}
+
 	return 0;
 }
